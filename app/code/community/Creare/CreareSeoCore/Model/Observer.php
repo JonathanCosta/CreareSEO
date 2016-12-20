c85de3f21c6e96fb925ef6ddffcc6372598d84b9
James Duke <jduke@ukpos.com>
2016-12-20 09:24:31 +0000

    fixed redirect loop wnhe url contains a & - introduced by Magento 1.9.3.1 upgrade

--- app/code/community/Creare/CreareSeoCore/Model/Observer.php
+++ app/code/community/Creare/CreareSeoCore/Model/Observer.php
@@ -187,7 +187,7 @@ class Creare_CreareSeoCore_Model_Observer extends Mage_Core_Model_Abstract {
                 $querystring = ($_SERVER['QUERY_STRING'] ? '?'.$_SERVER['QUERY_STRING'] : '');
                 $product = $observer->getEvent()->getProduct();
                 $url = $product->getUrlModel()->getUrl($product, array('_ignore_category'=>true)).$querystring;
-                if(Mage::helper('core/url')->getCurrentUrl() != $url){
+                if(Mage::helper('core/url')->getCurrentUrl() != Mage::helper('core/url')->escapeUrl($url)){
                     Mage::app()->getFrontController()->getResponse()->setRedirect($url,301);
                     Mage::app()->getResponse()->sendResponse();
                 }
